6f3918ffcdb63f0b8dfddc7225dcb137ff837fe4
mohitejaikumar <mohitejaikumar@gmail.com>
2024-10-31 02:58:39 +0530

    initialized backed with postgis

---
+++ prisma/migrations/20241030212608_added_postgis_extension/migration.sql
@@ -0,0 +1,22 @@
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "postgis";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone_number" INTEGER NOT NULL,
+    "gender" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "preferred_gender" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_number_key" ON "User"("phone_number");
